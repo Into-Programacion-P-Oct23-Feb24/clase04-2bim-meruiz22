95f3bba218d40a38970753294d33d556ff2e270e
meruiz22 <meruiz22@utpl.edu.ec>
2024-01-04 15:18:16 -0500

    cambios

--- ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
+++ ejemplos/Ejemplos/src/ejemplos02/Ejemplo041.java
@@ -13,7 +13,7 @@ public class Ejemplo041 {
 
     public static void main(String[] args) {
         // 
-        String cadena = "";
+        
         int[][] arreglo = {{1, 2, 3}, {3, 2, 4}, {2, 6, 2}};
         int[][] arreglo1 = {{1, 2, 3}, {2, 2, 2}, {3, 2, 1}};
         int[][] arreglo2 = new int[3][3];
@@ -26,7 +26,7 @@ public class Ejemplo041 {
         }
         for (int i = 0; i < arreglo1.length; i++) {
             for (int j = 0; j < arreglo1[i].length; j++) {
-                System.out.printf(" %s %d", cadena, arreglo2[i][j]);
+                System.out.printf("  %d", arreglo2[i][j]);
             }
             System.out.print("\n");
         }
